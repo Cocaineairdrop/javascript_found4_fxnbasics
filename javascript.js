fc65bd466a1b666a19c70e7d4444a6c253db7be8
Cocaineairdrop <madonna.s.murphy@gmail.com>
2025-01-13 20:28:51 -0800

    Finished the exercises

--- javascript.js
+++ javascript.js
@@ -5,12 +5,16 @@
 function add7(x) {
     return x + 7;
 }
-
-
+console.log(add7(3));
 
 /*2. Write a function called multiply that takes 2 numbers and 
     returns their product.
 */
+
+function multiply(a,b) {
+    return a * b;
+}
+console.log(multiply(2,3));
 
 /*3. Write a function called capitalize that takes a string and 
     returns that string with only the first letter capitalized. 
@@ -18,7 +22,18 @@ function add7(x) {
     UPPERCASE or BoTh.
 */
 
+function capitalize(string) {
+    const capitalized = string.charAt(0).toUpperCase() + string.slice(1);
+    return capitalized;
+}
+console.log(capitalize("Jarren is cute"));
+
 /*4. Write a function called lastLetter that takes a string 
     and returns the very last letter of that string:
   - lastLetter("abcd") should return "d" 
 */
+
+function lastLetter(string) {
+    return string.slice(-1);
+}
+console.log(lastLetter("Jarren is cute"));
